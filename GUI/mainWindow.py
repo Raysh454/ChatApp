e003f18ee8f73284ed2142440c2772c37754f61a
Huz <huzaifa.tatari@Gmail.com>
2024-01-19 07:46:59 +0500

    refined gui's look, added icon

--- GUI/mainWindow.py
+++ GUI/mainWindow.py
@@ -1,31 +1,59 @@
 import sys
 from ..Client import Client
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextEdit, QLineEdit, QPushButton, QMainWindow, QHBoxLayout
+from PyQt6.QtCore import QSize, QTimer, Qt
+from PyQt6.QtGui import QIcon, QShortcut
+import ctypes
 
-class chatApp(QWidget):
-    def __init__(self, host, port, parent=None):
-        super().__init__(parent)
+# This code makes it so windows doesn't use Pythonw.exe's icon in taskbar
+myappid = u'mycompany.myproduct.subproduct.version' # stupid windows
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-        self.client = Client(host, port)
-        self.client.connect()
+class chatApp(QMainWindow):
+    def __init__(self): # add host and port to init when done testing gui
+        super().__init__()
+        
+        self.setWindowTitle("Chat Application")
+        self.setMinimumSize(QSize(720,480))
+        self.setWindowIcon(QIcon('ChatApp/Icons/chat.png'))
+
+        #self.client = Client(host, port)
+        #self.client.connect()
 
         self.makeUI()
 
     def makeUI(self):
         layout = QVBoxLayout()
-
+        layoutH = QHBoxLayout()
         self.messageDisplay = QTextEdit(self)
         self.messageDisplay.setReadOnly(True)
         layout.addWidget(self.messageDisplay)
 
         self.inputBox = QLineEdit(self)
-        layout.addWidget(self.inputBox)
+        layoutH.addWidget(self.inputBox)
 
         sendButton = QPushButton('Send', self)
         sendButton.clicked.connect(self.sendMessage)
-        layout.addWidget(sendButton)
+        layoutH.addWidget(sendButton)
+        
 
-        self.client.response_received.connect(self.handleResponse)
+        #self.client.response_received.connect(self.handleResponse)
+
+        layoutH.setSpacing(10)
+        layoutH.setContentsMargins(0, 10, 0, 0)
+        layoutHWidget = QWidget()
+        layoutHWidget.setLayout(layoutH)
+
+        layout.addWidget(layoutHWidget)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        QTimer.singleShot(0, self.inputBox.setFocus)
+
+        self.inputBox.returnPressed.connect(self.sendMessage)
+        shortcut = QShortcut(Qt.Key.Key_Return, self.inputBox)
+        shortcut.activated.connect(self.sendMessage)
 
     def sendMessage(self):
         message = self.inputBox.text()
