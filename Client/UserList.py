779950f6395166271375f480c42ef3ac9dcf3c88
Raysh454 <67965243+Raysh454@users.noreply.github.com>
2024-02-07 14:54:01 +0500

    Update UserList.py

--- Client/UserList.py
+++ Client/UserList.py
@@ -7,7 +7,7 @@ class UserList:
     def __init__(self):
         self.head = None
     
-    def insert(self, username):
+    def add(self, username):
         newUser = User(username)
         if self.head is None:
             self.head = newUser
@@ -16,6 +16,21 @@ class UserList:
             while current.next is not None:
                 current = current.next
             current.next = newUser
+
+    def rem(self, username):
+        current = self.head
+
+        if current is not None and current.username == username:
+            self.head = current.next
+            return
+
+        prev = None
+        while current is not None and current.username != username:
+            prev = current
+            current = current.next
+
+        if current is not None:
+            prev.next = current.next
     
     def whatChanged(self, list_of_users):
         current = self.head
@@ -27,24 +42,38 @@ class UserList:
 
         new_usernames = set(list_of_users)
 
-        changes = []
+        added_username = new_usernames - current_usernames
+        removed_username = current_usernames - new_usernames
 
-        added_usernames = new_usernames - current_usernames
-        for username in added_usernames:
-            changes.append({"operation": "add", "username": username})
+        if added_username: return {'op': 'add', 'names': list(added_username)[0]}
+        if removed_username: return {'op': 'rem', 'names': list(removed_username)[0]}
+    
+    def printList(self):
+        current = self.head
+        user_list = []
 
-        removed_usernames = current_usernames - new_usernames
-        for username in removed_usernames:
-            changes.append({"operation": "rem", "username": username})
+        while current is not None:
+            user_list.append(current.username)
+            current = current.next
 
-        return changes
+        print(user_list, end='')
+    
+    def returnList(self):
+        current = self.head
+        user_list = []
+
+        while current is not None:
+            user_list.append(current.username)
+            current = current.next
+        return user_list
+
 if __name__ == "__main__":
     user_list = UserList()
-    user_list.insert("user1")
-    user_list.insert("user2")
-    user_list.insert("user3")
+    user_list.add("user1")
+    user_list.add("user2")
+    user_list.add("user3")
 
-    new_usernames = ["user1", "user2", "user3", "user4", "user5"]
+    new_usernames = ["user1", "user2"]
 
     changes = user_list.whatChanged(new_usernames)
     print(changes)
